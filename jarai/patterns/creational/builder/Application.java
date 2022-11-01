b014c38220cca9c44843b12cee3965714d18b87d
Viktor <viktor@nospam.org>
2022-11-01 11:23:40 +0100

    .

--- jarai/patterns/creational/builder/Application.java
+++ jarai/patterns/creational/builder/Application.java
@@ -2,22 +2,30 @@ package jarai.patterns.creational.builder;
 
 public class Application {
 
+
     public static void main(String[] args) {
+        Director director = new Director();
 
-        var auswahl = 1;
-        Koch koch;
+        // Director gets the concrete builder object from the client
+        // (application code). That's because application knows better which
+        // builder to use to get a specific product.
+        CarBuilder builder = new CarBuilder();
+        director.constructSportsCar(builder);
 
-        // Erbauer (Koch) auswaehlen
-        if (auswahl == 1)
-            koch = new ItalienischerKoch();
-        else
-            koch = new DeutscherKoch();
+        // The final product is often retrieved from a builder object, since
+        // Director is not aware and not dependent on concrete builders and
+        // products.
+        Car car = builder.getResult();
+        System.out.println("Car built:\n" + car.getCarType());
 
-        var kuechenChef = new RestaurantBesitzer(koch);
 
-        // Direktor erstellt eine komplexe Mahlzeit
-        kuechenChef.erstelleMahlzeit();
+        CarManualBuilder manualBuilder = new CarManualBuilder();
 
-        System.out.println(koch.getMahlzeit());
+        // Director may know several building recipes.
+        director.constructSportsCar(manualBuilder);
+        Manual carManual = manualBuilder.getResult();
+        System.out.println("\nCar manual built:\n" + carManual.print());
     }
+
 }
+
