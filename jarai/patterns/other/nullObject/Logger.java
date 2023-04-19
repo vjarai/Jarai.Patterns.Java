285faead77f152fcdc66f5dad78a72b35ea77746
Viktor <viktor@nospam.org>
2023-04-19 16:41:10 +0200

    Markup CommentsMarkup Comments

---
+++ jarai/patterns/other/nullObject/Logger.java
@@ -0,0 +1,7 @@
+package jarai.patterns.other.nullObject;
+
+public class Logger {
+    public void writeLog(String msg) {
+        System.out.println(msg);
+    }
+}
---
+++ jarai/patterns/other/nullObject/NullLogger.java
@@ -0,0 +1,8 @@
+package jarai.patterns.other.nullObject;
+
+public class NullLogger extends Logger {
+    @Override
+    public void writeLog(String msg) {
+        // Nothing to Do!
+    }
+}
